50237ce1993a4b8d1d60492ccfdde22897f2ac1e
gabrielnoda <gabrielnoda@hotmail.com>
2018-12-04 17:31:39 -0200

    (DB1 Start) Exercicios JAVA Atualizado

--- DB1Exercicios/exdb1java/ExerciciosJavaTest/src/br/com/db1/db1start/exercicios/Exercicios.java
+++ DB1Exercicios/exdb1java/ExerciciosJavaTest/src/br/com/db1/db1start/exercicios/Exercicios.java
@@ -1,104 +1,168 @@
 package br.com.db1.db1start.exercicios;
 
 public class Exercicios {
-	//1 - FUM que some dois números
+	// 1 - FUM que some dois números
 	public int somarDoisNumeros(int numeroUm, int numeroDois) {
 		return (numeroUm + numeroDois);
 	}
-	//2 - FUM que subtraia dois números
+
+	// 2 - FUM que subtraia dois números
 	public int subtrairDoisNumeros(int numeroUm, int numeroDois) {
 		return (numeroUm - numeroDois);
 	}
-	//3 - FUM que multiplique dois números 
+
+	// 3 - FUM que multiplique dois números
 	public int multiplicaDoisNumeros(int numeroUm, int numeroDois) {
 		return (numeroUm * numeroDois);
 	}
-	//4 - FUM que divida dois números
+
+	// 4 - FUM que divida dois números
 	public int divideDoisNumeros(int numeroUm, int numeroDois) {
 		return (numeroUm / numeroDois);
 	}
-	//5 - FUM que diga se o número é par
-	public int verificaNumeroPar(int numero){
-		if(numero % 2 == 0)
-		return 1;
+
+	// 5 - FUM que diga se o número é par
+	public Boolean verificaNumeroPar(int numero) {
+		if (numero % 2 == 0)
+			return true;
 		else
-			return 0;
+			return false;
 	}
-	//6 - FUM que receba dois números e diga qual é o maior
-	public int verificaQualEMaior (int numeroUm, int numeroDois) {
+
+	// 6 - FUM que receba dois números e diga qual é o maior
+	public int verificaQualEMaior(int numeroUm, int numeroDois) {
 		if (numeroUm > numeroDois)
 			return (numeroUm);
-			else
-				return(numeroDois);
+		else
+			return (numeroDois);
 	}
-	//7 - FUM que a partir de um valor inicial, mostre os números de ímpares até 100. 
-	public int numerosImparesAteCem (int numero) {
-		int x, quantidade;
-		quantidade = 0;
-		for(x = numero; x < 100; x++) {
+
+	// 7 - FUM que a partir de um valor inicial, mostre os números de ímpares até
+	// 100.
+	public int numerosImparesAteCem(int numero) {
+		int quantidade = 0;
+		for (int x = numero; x <= 100; x++) {
 			if (x % 2 == 1) {
 				quantidade++;
 			}
 		}
-		return (quantidade);
-		
+		return quantidade;
+
 	}
-	
-	//Strings
-	//1 - FUM que receba um texto e devolva ele em LETRAS MAIUSCULAS
+
+	// Strings
+	// 1 - FUM que receba um texto e devolva ele em LETRAS MAIUSCULAS
 	public String devolverMaiuscula(String texto) {
 		return texto.toUpperCase();
 	}
-	//2 - FUM que receba um texto e devolva ele em LETRAS MINUSCULAS
+
+	// 2 - FUM que receba um texto e devolva ele em LETRAS MINUSCULAS
 	public String devolverMinuscula(String texto) {
 		return texto.toLowerCase();
 	}
-	//3 - FUM que receba DB1START e retorne a quantidade de letras que existe nesta palavra
+
+	// 3 - FUM que receba DB1START e retorne a quantidade de letras que existe nesta
+	// palavra
 	public int tamanhoTexto(String texto) {
 		return texto.length();
 	}
-	//4 - FUM que receba DB1START (tendo um espaço no inicio e no fim da palavra) e retorne a quantidade de letras que existe
+
+	// 4 - FUM que receba DB1START (tendo um espaço no inicio e no fim da palavra) e
+	// retorne a quantidade de letras que existe
 	public int tamanhoTextoComEspacamento(String texto) {
-		String texto2 =  texto.trim();
+		String texto2 = texto.trim();
 		return texto2.length();
 	}
-	//5 - FUM que retorne o item 4 com a mesma quantidade de letras do item 3.
+
+	// 5 - FUM que retorne o item 4 com a mesma quantidade de letras do item 3.
 	public String retornarTextoSemEspacamento(String texto) {
 		return texto.trim();
 	}
-	//6 - FUM que receba o seu nome completo e exiba somente as 4 primeiras letras do seu nome
+
+	// 6 - FUM que receba o seu nome completo e exiba somente as 4 primeiras letras
+	// do seu nome
 	public String primeirasQuatroLetras(String nome) {
 		return nome.substring(0, 4);
 	}
-	//7 - FUM que receba o seu nome completo e exiba a partir da terceira letras do seu nome
+	// 7 - FUM que receba o seu nome completo e exiba a partir da terceira letras do
+	// seu nome
 
 	public String aPartirDaTerceiraLetra(String nome) {
 		return nome.substring(3);
 	}
-	//8 - FUM que receba o seu nome completo e exiba somente as 4 ultimas letras do seu nome
-	public String UltimasQuatroLetrasDoNome(String nome) {
-		return nome.substring(22);
+
+	// 8 - FUM que receba o seu nome completo e exiba somente as 4 ultimas letras do
+	// seu nome
+	public String ultimasQuatroLetrasDoNome(String nome) {
+		int tamanhoNome = nome.length();
+		return nome.substring(tamanhoNome - 4);
 	}
-	//9 - FUM que receba o seu nome completo e substitua o seu  primeiro nome por ALUNO/ALUNA
-	public String AlunoNoPrimeiroNome(String nome) {
-		String nome2 = nome.replace("Gabriel","Aluno");
-		return nome2;
+
+	// 9 - FUM que receba o seu nome completo e substitua o seu primeiro nome por
+	// ALUNO/ALUNA
+	public String alunoNoPrimeiroNome(String nome) {
+		String primeiroNome = "";
+		for (int i = 0; i < nome.length(); i++) {
+			char c = nome.charAt(i);
+			if (c == ' ')
+				i = 100;
+			else
+				primeiroNome += nome.charAt(i);
+		}
+		return nome.replace(primeiroNome, "Aluno");
+
 	}
-	//10 - FUM que receba o seguinte texto "banana, maçã, melancia" e exiba o texto separadamente.
-	//11 - FUM que receba um texto e exiba quantas vogais tem no texto
-	public int ContarVogais(String texto) {
+
+	// 10 - FUM que receba o seguinte texto "banana, maçã, melancia" e exiba o texto
+	// separadamente.
+	public String separarTexto(String texto) {
+		return texto.replace(",", "");
+	}
+
+	// 11 - FUM que receba um texto e exiba quantas vogais tem no texto
+	public int contarVogais(String texto) {
 		int quantidade = 0;
-		texto.toLowerCase();
+		String txt2 = texto.toLowerCase();
 		for (int i = 0; i < texto.length(); i++) {
-			char c = texto.charAt(i);
-			if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+			char c = txt2.charAt(i);
+			if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
 				quantidade++;
 		}
 		return quantidade;
 	}
+
+	// 12 - FUM que receba um texto e devolva ele invertido
+	public String textoInvertido(String texto) {
+		String invertida = new StringBuilder(texto).reverse().toString();
+		return invertida;
+	}
+
+	// Double
+	// 1 - FUM que exiba qual é o menor valor entre dois doubles
+	public Double menorValorDouble(Double valorUm, Double valorDois) {
+		if (valorUm < valorDois)
+			return valorUm;
+		else
+			return valorDois;
+	}
+
+	// 2 - FUM que exiba qual é o menor valor entre três doubles
+	public Double menorValoEntreTresDouble(Double valorUm, Double valorDois, Double valorTres) {
+		if (valorUm < valorDois && valorUm < valorTres)
+			return valorUm;
+		if (valorDois < valorUm && valorDois < valorTres)
+			return valorDois;
+		else
+			return valorTres;
+	}
+
+	// 3 - FUM que exiba a média de três números
+	public Double tirarMedia(Double valorUm, Double valorDois, Double valorTres) {
+		return (valorUm + valorDois + valorTres) / 3;
+	}
+
+	// 4 - FUM que calcule a área de um triangulo
+	public Double areaTriangulo(Double altura, Double base) {
+		return (altura * base) / 2;
+	}
 }
-		
-		
-	
-
-
--- DB1Exercicios/exdb1java/ExerciciosJavaTest/test/br/com/db1/db1start/exercicios/exerciciosTeste.java
+++ DB1Exercicios/exdb1java/ExerciciosJavaTest/test/br/com/db1/db1start/exercicios/exerciciosTeste.java
@@ -4,130 +4,221 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class exerciciosTeste {
-	//1 - FUM que some dois números
-	
+	// 1 - FUM que some dois números
 	@Test
 	public void deveSomarDoisNumeros() {
-	Exercicios exercicio = new Exercicios();
-	int Soma = exercicio.somarDoisNumeros(5, 4);
-	Assert.assertEquals(9,Soma);
+		Exercicios exercicio = new Exercicios();
+		int Soma = exercicio.somarDoisNumeros(5, 4);
+		Assert.assertEquals(9, Soma);
 	}
-	 //2 - FUM que subtraia dois números
+
+	// 2 - FUM que subtraia dois números
 	@Test
 	public void deveSubtrairDoisNumeros() {
-	Exercicios exercicio = new Exercicios();
-	int subtrair = exercicio.subtrairDoisNumeros(5, 4);
-	Assert.assertEquals(1,subtrair);
+		Exercicios exercicio = new Exercicios();
+		int subtrair = exercicio.subtrairDoisNumeros(5, 4);
+		Assert.assertEquals(1, subtrair);
 	}
-	//3 - FUM que multiplique dois números 
+
+	// 3 - FUM que multiplique dois números
 	@Test
 	public void deveMultiplicarDoisNumeros() {
-	Exercicios exercicio = new Exercicios();
-	int resultadoMultiplicacao = exercicio.multiplicaDoisNumeros(5, 4);
-	Assert.assertEquals(20,resultadoMultiplicacao);
+		Exercicios exercicio = new Exercicios();
+		int resultadoMultiplicacao = exercicio.multiplicaDoisNumeros(5, 4);
+		Assert.assertEquals(20, resultadoMultiplicacao);
 	}
-	//4 - FUM que divida dois números
+
+	// 4 - FUM que divida dois números
 	@Test
 	public void deveDividirDoisNumeros() {
-	Exercicios exercicio = new Exercicios();
-	int resultadoDivisao = exercicio.divideDoisNumeros(6, 2);
-	Assert.assertEquals(3,resultadoDivisao);
+		Exercicios exercicio = new Exercicios();
+		int resultadoDivisao = exercicio.divideDoisNumeros(6, 2);
+		Assert.assertEquals(3, resultadoDivisao);
 	}
-	//5 - FUM que diga se o número é par
+
+	// 5 - FUM que diga se o número é par
 	@Test
 	public void deveVerificarNumeroPar() {
-	Exercicios exercicio = new Exercicios();
-	int verificaPar = exercicio.verificaNumeroPar(6);
-	Assert.assertEquals(1, verificaPar);
+		Exercicios exercicio = new Exercicios();
+		Boolean verificaNumero = exercicio.verificaNumeroPar(6);
+		Assert.assertTrue(verificaNumero);
 	}
-	//6 - FUM que receba dois números e diga qual é o maior
+
+	// false
+	@Test
+	public void deveVerificarNumeroImpar() {
+		Exercicios exercicio = new Exercicios();
+		Boolean verificaNumero = exercicio.verificaNumeroPar(5);
+		Assert.assertFalse(verificaNumero);
+	}
+
+	// 6 - FUM que receba dois números e diga qual é o maior
 	@Test
 	public void deveRetornarMaiorNumero() {
-	Exercicios exercicio = new Exercicios();
-	int maiorNumero = exercicio.verificaQualEMaior(5,9);
-	Assert.assertEquals(9, maiorNumero);
+		Exercicios exercicio = new Exercicios();
+		int maiorNumero = exercicio.verificaQualEMaior(5, 9);
+		Assert.assertEquals(9, maiorNumero);
 	}
-	//7 - FUM que a partir de um valor inicial, mostre os números de ímpares até 100. 
+
+	// 7 - FUM que a partir de um valor inicial, mostre os números de ímpares até
+	// 100.
 	@Test
 	public void deveMostrarNumerosImparesAteCem() {
-	Exercicios exercicio = new Exercicios();
-	int numerosImpar = exercicio.numerosImparesAteCem(90);
-	Assert.assertEquals(5, numerosImpar);
+		Exercicios exercicio = new Exercicios();
+		int numerosImpar = exercicio.numerosImparesAteCem(90);
+		Assert.assertEquals(5, numerosImpar);
 	}
-	
-	//Strings
-	//1 - FUM que receba um texto e devolva ele em LETRAS MAIUSCULAS
+
+	// Strings
+	// 1 - FUM que receba um texto e devolva ele em LETRAS MAIUSCULAS
 	@Test
 	public void deveDeixarTextoMaiusculo() {
-	Exercicios exercicio = new Exercicios();
-	String maiusculo = exercicio.devolverMaiuscula("texto maiusculo");
-	Assert.assertEquals("TEXTO MAIUSCULO", maiusculo);
+		Exercicios exercicio = new Exercicios();
+		String maiusculo = exercicio.devolverMaiuscula("texto maiusculo");
+		Assert.assertEquals("TEXTO MAIUSCULO", maiusculo);
 	}
-	//2 - FUM que receba um texto e devolva ele em LETRAS MINUSCULAS
+
+	// 2 - FUM que receba um texto e devolva ele em LETRAS MINUSCULAS
 	@Test
 	public void deveDeixarTextoMinusculo() {
-	Exercicios exercicio = new Exercicios();
-	String minuscula = exercicio.devolverMinuscula("TEXTO MINUSCULA");
-	Assert.assertEquals("texto minuscula", minuscula);
+		Exercicios exercicio = new Exercicios();
+		String minuscula = exercicio.devolverMinuscula("TEXTO MINUSCULA");
+		Assert.assertEquals("texto minuscula", minuscula);
 	}
-	//3 - FUM que receba DB1START e retorne a quantidade de letras que existe nesta palavra
+
+	// 3 - FUM que receba DB1START e retorne a quantidade de letras que existe nesta
+	// palavra
 	@Test
 	public void deveDevolverQuantidadeDeLetras() {
-	Exercicios exercicio = new Exercicios();
-	int quantidaDeLetras = exercicio.tamanhoTexto("DB1START");
-	Assert.assertEquals(8, quantidaDeLetras);
+		Exercicios exercicio = new Exercicios();
+		int quantidaDeLetras = exercicio.tamanhoTexto("DB1START");
+		Assert.assertEquals(8, quantidaDeLetras);
 	}
-	//4 - FUM que receba DB1START (tendo um espaço no inicio e no fim da palavra) e retorne a quantidade de letras que existe 
+
+	// 4 - FUM que receba DB1START (tendo um espaço no inicio e no fim da palavra) e
+	// retorne a quantidade de letras que existe
 	@Test
 	public void deveDevolverQuantidadeDeLetrasComEspacamento() {
-	Exercicios exercicio = new Exercicios();
-	int quantidaDeLetrasEspaco = exercicio.tamanhoTextoComEspacamento(" DB1START ");
-	Assert.assertEquals(8, quantidaDeLetrasEspaco);
+		Exercicios exercicio = new Exercicios();
+		int quantidaDeLetrasEspaco = exercicio.tamanhoTextoComEspacamento(" DB1START ");
+		Assert.assertEquals(8, quantidaDeLetrasEspaco);
 	}
-	//5 - FUM que retorne o item 4 com a mesma quantidade de letras do item 3.
+
+	// 5 - FUM que retorne o item 4 com a mesma quantidade de letras do item 3.
 	@Test
 	public void deveDevolverTextoSemEspacamento() {
-	Exercicios exercicio = new Exercicios();
-	String textoSemEspacamento = exercicio.retornarTextoSemEspacamento (" DB1START ");
-	Assert.assertEquals("DB1START", textoSemEspacamento);
+		Exercicios exercicio = new Exercicios();
+		String textoSemEspacamento = exercicio.retornarTextoSemEspacamento(" DB1START ");
+		Assert.assertEquals("DB1START", textoSemEspacamento);
 	}
-	//6 - FUM que receba o seu nome completo e exiba somente as 4 primeiras letras do seu nome
+
+	// 6 - FUM que receba o seu nome completo e exiba somente as 4 primeiras letras
+	// do seu nome
 	@Test
 	public void deveRetornarPrimeirasQuatroLetras() {
-	Exercicios exercicio = new Exercicios();
-	String primeirasQuatroLetras = exercicio.primeirasQuatroLetras("Gabriel Eid Umbelino Noda");
-	Assert.assertEquals("Gabr", primeirasQuatroLetras);
+		Exercicios exercicio = new Exercicios();
+		String primeirasQuatroLetras = exercicio.primeirasQuatroLetras("Gabriel Eid Umbelino Noda");
+		Assert.assertEquals("Gabr", primeirasQuatroLetras);
 	}
-	//7 - FUM que receba o seu nome completo e exiba a partir da terceira letras do seu nome
+
+	// 7 - FUM que receba o seu nome completo e exiba a partir da terceira letras do
+	// seu nome
 	@Test
 	public void deveRetornarNomeApos3Letra() {
-	Exercicios exercicio = new Exercicios();
-	String textoDepoisDa3 = exercicio.aPartirDaTerceiraLetra("Gabriel Eidi Umbelino Noda");
-	Assert.assertEquals("riel Eidi Umbelino Noda", textoDepoisDa3);
+		Exercicios exercicio = new Exercicios();
+		String textoDepoisDa3 = exercicio.aPartirDaTerceiraLetra("Gabriel Eidi Umbelino Noda");
+		Assert.assertEquals("riel Eidi Umbelino Noda", textoDepoisDa3);
 	}
-	//8 - FUM que receba o seu nome completo e exiba somente as 4 ultimas letras do seu nome
+
+	// 8 - FUM que receba o seu nome completo e exiba somente as 4 ultimas letras do
+	// seu nome
 	@Test
 	public void deveRetornarUltimas4Letras() {
-	Exercicios exercicio = new Exercicios();
-	String Ultimas4 = exercicio.UltimasQuatroLetrasDoNome("Gabriel Eidi Umbelino Noda");
-	Assert.assertEquals("Noda", Ultimas4);
+		Exercicios exercicio = new Exercicios();
+		String ultimas4 = exercicio.ultimasQuatroLetrasDoNome("Gabriel Eidi Umbelino Noda");
+		Assert.assertEquals("Noda", ultimas4);
 	}
-	//9 - FUM que receba o seu nome completo e substitua o seu  primeiro nome por ALUNO/ALUNA
+
+	// 9 - FUM que receba o seu nome completo e substitua o seu primeiro nome por
+	// ALUNO/ALUNA
 	@Test
 	public void devePrimeiroNomeSubstituidoPorAluno() {
-	Exercicios exercicio = new Exercicios();
-	String Aluno = exercicio.AlunoNoPrimeiroNome("Gabriel Eidi Umbelino Noda");
-	Assert.assertEquals("Aluno Eidi Umbelino Noda", Aluno);
+		Exercicios exercicio = new Exercicios();
+		String aluno = exercicio.alunoNoPrimeiroNome("Leonardo Takeshi");
+		Assert.assertEquals("Aluno Takeshi", aluno);
 	}
-	//10 - FUM que receba o seguinte texto "banana, maçã, melancia" e exiba o texto separadamente.
-	//11 - FUM que receba um texto e exiba quantas vogais tem no texto
+
+	// 10 - FUM que receba o seguinte texto "banana, maçã, melancia" e exiba o texto
+	// separadamente.
+	@Test
+	public void deveSepararTexto() {
+		Exercicios exercicio = new Exercicios();
+		String separado = exercicio.separarTexto("banana, maçã, melancia");
+		Assert.assertEquals("banana maçã melancia", separado);
+	}
+
+	// 11 - FUM que receba um texto e exiba quantas vogais tem no texto
 	@Test
 	public void deveContarVogais() {
-	Exercicios exercicio = new Exercicios();
-	int vogais = exercicio.ContarVogais("Gabriel Eidi Umbelino Noda");
-	System.out.println(vogais);
-	Assert.assertEquals(11 , vogais);
+		Exercicios exercicio = new Exercicios();
+		int vogais = exercicio.contarVogais("pArAlElEpIpEdOo");
+		Assert.assertEquals(8, vogais);
+	}
+
+	// 12 - FUM que receba um texto e devolva ele invertido
+	@Test
+	public void deveInverterTexto() {
+		Exercicios exercicio = new Exercicios();
+		String invertido = exercicio.textoInvertido("noda");
+		Assert.assertEquals("adon", invertido);
+	}
+
+	// Double
+	// 1 - FUM que exiba qual é o menor valor entre dois doubles
+	@Test
+	public void deveRetornarMenorValorDouble() {
+		Exercicios exercicio = new Exercicios();
+		Double menorDouble = exercicio.menorValorDouble(1.55, 3.12);
+		Assert.assertEquals(1.55, menorDouble, 0);
+	}
+
+	// 2 - FUM que exiba qual é o menor valor entre três doubles
+	@Test
+	public void deveRetornarMenorValorDoubleEntreTres() {
+		Exercicios exercicio = new Exercicios();
+		Double menorDouble3 = exercicio.menorValoEntreTresDouble(1.60, 6.21, 0.52);
+		Assert.assertEquals(0.52, menorDouble3, 0);
+	}
+
+	// TEST DOIS
+	@Test
+	public void deveRetornarMenorValorDoubleEntreTres2() {
+		Exercicios exercicio = new Exercicios();
+		Double menorDouble3 = exercicio.menorValoEntreTresDouble(1.60, 0.52, 6.21);
+		Assert.assertEquals(0.52, menorDouble3, 0);
+	}
+
+	// TEST TRES
+	@Test
+	public void deveRetornarMenorValorDoubleEntreTres3() {
+		Exercicios exercicio = new Exercicios();
+		Double menorDouble3 = exercicio.menorValoEntreTresDouble(0.52, 1.60, 6.21);
+		Assert.assertEquals(0.52, menorDouble3, 0);
+	}
+
+	// 3 - FUM que exiba a média de três números
+	@Test
+	public void deveRetornarMedia() {
+		Exercicios exercicio = new Exercicios();
+		Double media = exercicio.tirarMedia(5.0, 7.0, 4.0);
+		Assert.assertEquals(5.33, media, 0.9);
+	}
+
+	// 4 - FUM que calcule a área de um triangulo
+	@Test
+	public void deveRetornarAreaTriangulo() {
+		Exercicios exercicio = new Exercicios();
+		Double areaTriangulo = exercicio.areaTriangulo(5.5, 10.0);
+		Assert.assertEquals(27.5, areaTriangulo, 0.9);
 	}
 }
-
-
